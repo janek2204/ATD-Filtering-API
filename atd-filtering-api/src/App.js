fa26b1cfc4ba031126619d233f139ecc5213d177
piotr <piotr.jankowski2204@gmail.com>
2022-03-04 15:34:03 +0000

    improvements on conditional renedrings

--- atd-filtering-api/src/App.js
+++ atd-filtering-api/src/App.js
@@ -60,34 +60,42 @@ function App() {
         <br />
         {error.length > 0 ? <h1>Title do not exist!</h1> : ""}
         <br />
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Destination</th>
-              <th>Adult: Price From</th>
-              <th>Child: Price From</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((advert) => {
-              return (
-                <tr key={advert.id}>
-                  <td>
-                    <img src={advert.img_sml} alt={advert.dest} />
-                  </td>
-                  <td>{advert.title}</td>
-                  <td>{advert.dest}</td>
-                  <td>{advert.price_from_adult}</td>
-                  <td>{advert.price_from_child}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+        {data.length ? (
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Destination</th>
+                <th>Adult: Price From</th>
+                <th>Child: Price From</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((advert) => {
+                return (
+                  <tr key={advert.id}>
+                    <td>
+                      <img src={advert.img_sml} alt={advert.dest} />
+                    </td>
+                    <td>{advert.title}</td>
+                    <td>{advert.dest}</td>
+                    <td>{advert.price_from_adult}</td>
+                    <td>
+                      {!advert.price_from_child.length
+                        ? "N/A"
+                        : advert.price_from_child}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        ) : (
+          ""
+        )}
         <br />
-        {data.length <= 0 ? (
+        {!data.length ? (
           ""
         ) : (
           <Button variant="info" onClick={handleLimit}>
