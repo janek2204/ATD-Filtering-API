8d81dbfe171bc31b1dcd27fe1d2dad3dc64a3551
piotr <piotr.jankowski2204@gmail.com>
2022-03-05 10:50:54 +0000

    filtering working as it should, lodaing more adverts working as it should

--- atd-filtering-api/src/App.js
+++ atd-filtering-api/src/App.js
@@ -9,50 +9,62 @@ import Table from "react-bootstrap/Table";
 
 function App() {
   const [data, setData] = useState([]);
+  const [newData, setNewData] = useState([]);
+
   const [title, setTitle] = useState("");
   const [error, setError] = useState("");
-  const [offset, setOffset] = useState(1);
+  const [offset, setOffset] = useState(0);
   const [meta, setMeta] = useState([]);
-  const [search, setSearch] = useState(true);
+  const [filtering, setFiltering] = useState([]);
+
+  const getData = async () => {
+    try {
+      const {
+        data: { data },
+        data: { meta },
+      } = await axios.get(
+        `https://global.atdtravel.com/api/products?geo=en&offset=${offset}&limit=10`
+      );
+      if (newData.length <= 0) setNewData(data);
+      else setData(data);
+      setMeta(meta);
+    } catch (err) {
+      return setError(err.message);
+    }
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const {
-          data: { data },
-          data: { meta },
-        } = await axios.get(
-          `https://global.atdtravel.com/api/products?geo=en&offset=${offset}&title=${title}`
-        );
-        setData(data);
-        setMeta(meta);
-        setError("");
-      } catch (err) {
-        return setError(err.message);
-      }
-    };
     getData();
-  }, [offset, search]);
+  }, [offset]);
 
   const handleTyping = (event) => {
     setTitle(event.target.value);
   };
 
-  const handlePreviousPage = () => {
-    setOffset(offset - 10);
-  };
-
   const handleNextPage = () => {
-    setOffset(offset + 10);
+    setOffset((offset) => offset + 10);
+    setNewData(newData.concat(data));
   };
 
-  const handleSearch = () => {
-    setSearch(!search);
+  const handleFiltering = () => {
+    const toLower = title.toLowerCase();
+
+    const filter = newData.filter((advert) => {
+      const advertToLower = advert.title.toLowerCase();
+      if (advertToLower.includes(toLower)) {
+        return advert;
+      }
+    });
+    setFiltering(filter);
   };
+
   return (
     <>
       <Container>
         <h1>Product search</h1>
+        <p>newData length {newData.length}</p>
+        <p>offset {offset}</p>
+        <p>Filtering array length: {filtering.length}</p>
         <InputGroup className="mb-3">
           <FormControl
             placeholder="Type here...."
@@ -65,7 +77,7 @@ function App() {
             <Button
               variant="outline-secondary"
               id="button-addon2"
-              onClick={handleSearch}
+              onClick={handleFiltering}
             >
               Search
             </Button>
@@ -73,7 +85,7 @@ function App() {
         </InputGroup>
         <br />
         {error.length > 0 ? <h1>Title do not exist!</h1> : ""}
-        {data.length ? (
+        {!filtering.length ? (
           <Table striped bordered hover variant="dark">
             <thead>
               <tr>
@@ -90,49 +102,89 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {data.map((advert) => {
-                return (
-                  <tr key={advert.id}>
-                    <td>
-                      <img src={advert.img_sml} alt={advert.dest} />
-                    </td>
-                    <td>{advert.title}</td>
-                    <td>{advert.dest}</td>
-                    <td>{advert.price_from_adult}</td>
-                    <td>
-                      {!advert.price_from_child.length
-                        ? "N/A"
-                        : advert.price_from_child}
-                    </td>
-                  </tr>
-                );
-              })}
+              {newData.length <= 0
+                ? data.map((advert) => {
+                    return (
+                      <tr key={advert.id}>
+                        <td>
+                          <img src={advert.img_sml} alt={advert.dest} />
+                        </td>
+                        <td>{advert.title}</td>
+                        <td>{advert.dest}</td>
+                        <td>{advert.price_from_adult}</td>
+                        <td>
+                          {!advert.price_from_child
+                            ? "N/A"
+                            : advert.price_from_child}
+                        </td>
+                      </tr>
+                    );
+                  })
+                : newData.map((advert) => {
+                    return (
+                      <tr key={advert.id}>
+                        <td>
+                          <img src={advert.img_sml} alt={advert.dest} />
+                        </td>
+                        <td>{advert.title}</td>
+                        <td>{advert.dest}</td>
+                        <td>{advert.price_from_adult}</td>
+                        <td>
+                          {!advert.price_from_child
+                            ? "N/A"
+                            : advert.price_from_child}
+                        </td>
+                      </tr>
+                    );
+                  })}
             </tbody>
           </Table>
         ) : (
-          ""
+          <>
+            <h1>We have {filtering.length} atractions for you!</h1> <br />
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Destination</th>
+                  <th>
+                    Adult: <br />
+                    Price From
+                  </th>
+                  <th>
+                    Child: <br /> Price From
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {filtering.map((advert) => {
+                  return (
+                    <tr key={advert.id}>
+                      <td>
+                        <img src={advert.img_sml} alt={advert.dest} />
+                      </td>
+                      <td>{advert.title}</td>
+                      <td>{advert.dest}</td>
+                      <td>{advert.price_from_adult}</td>
+                      <td>
+                        {!advert.price_from_child.length
+                          ? "N/A"
+                          : advert.price_from_child}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </>
         )}
         <br />
-        {!data.length ? (
-          ""
-        ) : (
-          <div className="pagination-buttons">
-            {offset <= 0 ? (
-              ""
-            ) : (
-              <Button variant="info" onClick={handlePreviousPage} size="lg">
-                Previous page
-              </Button>
-            )}
-            {offset < meta.total_count ? (
-              <Button variant="primary" onClick={handleNextPage} size="lg">
-                Next page
-              </Button>
-            ) : (
-              ""
-            )}
-          </div>
-        )}
+        <div className="pagination-buttons">
+          <Button variant="primary" onClick={handleNextPage} size="lg">
+            More results
+          </Button>
+        </div>
         <br />
       </Container>
     </>
